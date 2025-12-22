4ee2fb8110d4bd23500c97ab69ee7a14cdb5c3e4
ablyNavreen <navreen.kaur@fatbit.in>
2025-12-22 15:53:30 +0530

    fixed image rotation and token issue in chat

--- app/src/main/java/com/example/firebasechattingapplication/utils/Constants.kt
+++ app/src/main/java/com/example/firebasechattingapplication/utils/Constants.kt
@@ -14,7 +14,7 @@ object Constants {
     const val USER_EMAIL = "user_email"
     const val USER_GENDER = "user_gender"
     const val USER_TOKEN = "user_token"
-    const val OAUTH_ACCESS_TOKEN = "ya29.a0Aa7pCA8ZufIZnSIZmbpz_llM8nEi3DbwhBMZLegOIBAFwBissyBFV-Yvul6KEL1k0YmIJD9BXMVI_LIiBF61_SCpVjb3fdIVIANImEv7J8j_FkozKf0H_QpP85syxnUDtvgY6OhD9fdWWdYWtAq0q4SAsm-CGzJAaeTEeK8BiVEz60eSW-aJPWgVtRvVq3TjEZQpXGsaCgYKAZ0SARMSFQHGX2MiEUfILO_K8sX7hey0_JygGQ0206"
+    const val OAUTH_ACCESS_TOKEN = ""
     const val FCM_BASE_URL = "https://fcm.googleapis.com/"
     const val SEND_PUSH = "v1/projects/fir-chattingapplication-1de73/messages:send"
 
